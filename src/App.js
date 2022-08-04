b5b2a1ba4454ae69f3d8d0b5e1a590f2e1193dc7
jjw2596 <80455921+jjw2596@users.noreply.github.com>
2022-08-04 22:51:22 +0900

    start

---
+++ src/App.js
@@ -0,0 +1,16 @@
+import React from 'react';
+import TodoTemplate from './component/TodoTemplate';
+import TodoInsert from './component/TodoInsert';
+import TodoList from './component/TodoList';
+
+const App = () => {
+  return (
+    <TodoTemplate>
+      <TodoInsert />
+        <TodoList />
+      </TodoTemplate>
+
+  )
+}
+
+export default App;
